3025c0247748ab2174e36ad4ca5203f17d198dbb
Piotr Gdański <pgdanski@gmail.com>
2024-11-19 13:58:40 +0100

    fix models and settings

--- classroom/models.py
+++ classroom/models.py
@@ -2,31 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-USER_TYPE_CHOICES = {
-    "ST": "STUFF",
-    "SU": "STUDENT", 
-}
+USER_TYPE_CHOICES = [
+    ("ST", "STUFF"),
+    ("SU", "STUDENT"), 
+]
 
-DAY_CHOICES = {
-    "MON": "MONDAY",
-    "TUE": "TUESDAY",
-    "WED": "WEDNESDAY",
-    "THU": "THURSDAY",
-    "FRI": "FRIDAY",
-}
+DAY_CHOICES = [
+    ("MON", "MONDAY"),
+    ("TUE", "TUESDAY"),
+    ("WED", "WEDNESDAY"),
+    ("THU", "THURSDAY"),
+    ("FRI", "FRIDAY"),
+]
 
 class School(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254    )
+    email = models.EmailField(max_length=254)
 
 class Class(models.Model):
     name = models.CharField(max_length=100)
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='classes')
     
-
 class User(AbstractUser):
     type = models.CharField(max_length=3, choices=USER_TYPE_CHOICES)
-    group = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="class_studensts")
+    group = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="class_students")
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="school_students")
 
 class Lesson(models.Model):
@@ -34,18 +33,18 @@ class Lesson(models.Model):
     day = models.CharField(max_length=3, choices=DAY_CHOICES)
     time_start = models.TimeField()
     time_end = models.TimeField()
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lessons")
-    students = models.ManyToManyField(User, on_delete=models.CASCADE, related_name="lessons")
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="teacher_lessons")
+    students = models.ManyToManyField(User, related_name="student_lessons")
 
 class Homework(models.Model):
     subject = models.CharField(max_length=300)
-    content = models.CharField()
+    content = models.CharField(max_length=10000)
     deadline = models.DateField()
     group = models.ForeignKey(Class, on_delete=models.CASCADE, related_name="homeworks")
 
 class homework_submission(models.Model):
-    homework = models.ForeignKey(Homework, on_delete=models.CASCADE, related_name="submissions")
-    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="submissions")
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE, related_name="students_submissions")
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_submissions")
     content = models.CharField(max_length=10000)
 
     
